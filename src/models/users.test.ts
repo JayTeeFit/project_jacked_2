07e4aef239ba52ff50efafe55eb379ba9b925db6
Russell Engebretson <84556952+rengebre@users.noreply.github.com>
2024-01-13 20:50:58 -0800

    minor update

--- src/models/users.test.ts
+++ src/models/users.test.ts
@@ -2,17 +2,19 @@ import { StartedPostgreSqlContainer } from "@testcontainers/postgresql";
 import { NodePgDatabase } from "drizzle-orm/node-postgres/driver";
 import { Client } from "pg";
 import { NewUserSchema, users } from "src/db/schema";
-import { setupDockerTestDb } from "src/test_helpers/setup_docker_test_db";
+import {
+  setupDockerTestDb,
+  DEFAULT_TEST_TIMEOUT,
+} from "src/test_helpers/setup_docker_test_db";
 
 describe("Users", () => {
-  const defaultTimeout = 60 * 1000;
   let container: StartedPostgreSqlContainer;
   let db: NodePgDatabase<Record<string, never>>;
   let client: Client;
 
   beforeAll(async () => {
     ({ container, db, client } = await setupDockerTestDb());
-  }, defaultTimeout);
+  }, DEFAULT_TEST_TIMEOUT);
 
   it("can insert a user", async () => {
     const newUser: NewUserSchema = {
